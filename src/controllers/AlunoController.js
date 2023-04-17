cbbfcf6beb68e2627f4b2165f15d62731eccd43f
Marco Silva <marco.silva.ext@brmalls.com.br>
2023-04-17 15:00:00 -0300

    Alunos routers

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,101 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const newAluno = await Aluno.create(req.body);
+
+      return res.json(newAluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id is required'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno not exist'],
+        });
+      }
+
+      const updateAluno = await aluno.update(req.body);
+
+      return res.json(updateAluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id is required'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno not exist'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id is required'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno not exist'],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json({
+        delete: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
