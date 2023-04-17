b823f296d17b2a95f103610685ca962963412858
Marco Silva <marco.silva.ext@brmalls.com.br>
2023-04-17 13:38:39 -0300

    Alunos Routes inserted

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,15 +2,7 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Marco Antonio',
-      sobrenome: 'Rodrigues da Silva',
-      email: 'teste@teste.com',
-      idade: 21,
-      peso: 85.2,
-      altura: 1.76,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
